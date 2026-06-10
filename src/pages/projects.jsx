7e89336cd158dbd72cd4422a4b5110c181b578d6
Haitham Z <haithamzitioui@gmail.com>
2026-06-10 14:59:52 -0400

    fixed error where projects table had members' names without spaces

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -191,11 +191,11 @@ export default function Projects(){
         {id: 3, projectName: "Epic We Need to Talk", members: ["Jake ", "Justin ", "Kowalski ", "Alisson"], completion: "100%"},
         {id: 4, projectName: "Droid Tycoon Droid Duplication Glitch", members: ["Sam ", "Caden ", "Abraham"], completion: "25%"},
         {id: 5, projectName: "Droid Tycoon is Hiding this From You", members:["Jack ", "Jill"], completion: "45%"},
-        {id: 6, projectName: "Sick of Brainrot Games, Play This Instead", members:["Benjamin", "Jason", "Clark", "Donald"], completion: "15%"},
+        {id: 6, projectName: "Sick of Brainrot Games, Play This Instead", members:["Benjamin ", "Jason ", "Clark ", "Donald"], completion: "15%"},
         {id: 7, projectName: "Droid Tycoon is Hiding this From You", members:["Jack ", "Jill"], completion: "45%"},
-        {id: 8, projectName: "Droid Tycoon Mr Bones Event is Here!", members:["Quentin", "Michael", "Hassan", "Julia"], completion: "89%"},
-        {id: 9, projectName: "I Turned My House into a Swimming Pool", members:["Ryan", "Andrew", "Kaleb", "Ava", "Craig", "Gillian"], completion: "5%"},
-        {id: 10, projectName: "I Coded Minecraft to Be More Satisfying", members:["Talia", "Killian", "George"], completion: "11%"}
+        {id: 8, projectName: "Droid Tycoon Mr Bones Event is Here!", members:["Quentin ", "Michael ", "Hassan ", "Julia"], completion: "89%"},
+        {id: 9, projectName: "I Turned My House into a Swimming Pool", members:["Ryan ", "Andrew ", "Kaleb ", "Ava ", "Craig ", "Gillian"], completion: "5%"},
+        {id: 10, projectName: "I Coded Minecraft to Be More Satisfying", members:["Talia ", "Killian ", "George"], completion: "11%"}
     ])
 
     const [foundProject, setFoundProject] = useState(null)
